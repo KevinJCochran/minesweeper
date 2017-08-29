39f7d10cf7f1212774a6b1fa64e94d7e3d93f95f
Kevin <kevin.cochran@rutgers.edu>
2017-08-29 17:29:15 -0400

    generatePlayerBoard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,22 @@
 // Minesweeper.js
 // Kevin Cochran
 
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++) {
+        board.push([]);
+        for(let j = 0; j < numberOfColumns; j++) {
+            board[i].push(' ');
+        }
+    }
+    return board;
+}
 const printBoard = board => {
     console.log('Current board: ');
-    console.log(board[0].join('|'));
+    for(let i = 0; i < board.length; i++) {
+        console.log(board[i].join('|'));
+    }
 }
 
-
-let board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-]
-console.log(printBoard(board));
+// console.log(generatePlayerBoard(5,4));
+printBoard(generatePlayerBoard(5,4));
